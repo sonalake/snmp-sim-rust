6f30cf880242eeb0d3bc3ab0e534c58f16bf3b6f
frankhorv <fhorvath80@gmail.com>
2022-04-27 14:00:53 +0200

    Fix swagger

--- docs/dist/swagger-initializer.js
+++ docs/dist/swagger-initializer.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+window.onload = function () {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: "openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
