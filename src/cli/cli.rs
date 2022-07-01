3e083a40cfb2c4688173162a24ce8a4e36ad12be
Conor Kinsella <conor.kinsella@scully.local>
2022-07-01 16:09:01 +0100

    cli.rs incomplete

--- src/cli/cli.rs
+++ src/cli/cli.rs
@@ -4,8 +4,10 @@ use clap::Parser;
 fn main() -> Result<(), serde::yaml::Error> {
 
     // Insert code for variable holding default values
+    let settings = Settings::default();
 
     // Insert code for serializing variable
-    // e.g. let s = serde_yaml::to_string(&<variable_name>);
+    let s = serde_yaml::to_string(&settings)?;
 
+    Ok(())
 }
