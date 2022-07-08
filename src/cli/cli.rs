cc33d25a1354af7079c9fbe5ec59c6a79b96a408
Conor Kinsella <conor.kinsella@scully.local>
2022-07-08 11:27:44 +0100

    File overwrites if exists

--- src/cli/cli.rs
+++ src/cli/cli.rs
@@ -1,9 +1,10 @@
-use configuration::Settings;
 use clap::Parser;
-use std::io::prelude::*;
+use configuration::Settings;
 use std::fs::File;
-use std::path::Path;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 use std::io::ErrorKind;
+use std::path::Path;
 
 // fn write_data_to_file() -> Result<(), serde::yaml::Error> {
 
@@ -33,8 +34,43 @@ use std::io::ErrorKind;
 //     Ok(())
 // }
 
-fn generate_default_config(overwrite:bool) -> Result<(), serde::yaml::Error> {
+// fn generate_default_config(overwrite:bool) -> Result<(), serde::yaml::Error> {
 
+//     // Path to file
+//     let path = Path::new("./configuration/base.yaml");
+
+//     // Insert code for variable holding default values
+//     let settings = Settings::default();
+
+//     // Insert code for serializing variable
+//     let s = serde_yaml::to_string(&settings)?;
+
+//     // Creates file in directory
+//     let f = File::create(&path);
+
+//     // match statement, if file creates
+//     // let f = match f {
+//     //     Ok(file) => file,
+//     //     Err(error) => panic!("This file already exists {:?}", error),
+//     // };
+
+//     let f = match f {
+//         Ok(file) => file,
+//         Err(error) => match error.kind() {
+//             // ErrorKind::AlreadyExists => match File::open(&path) {
+//             //     Ok(fo) => {
+//             //         if overwrite == True {
+//             //             fo,
+//             //         };
+//             //     }
+//             ErrorKind::AlreadyExists if overwrite => File::open(path)?;
+
+//             Err(err) => Err("This file already exists {:?}", err);
+//         },
+//     };
+// }
+
+fn write_default_config(overwrite: bool) -> Result<(), WriteConfigError> {
     // Path to file
     let path = Path::new("./configuration/base.yaml");
 
@@ -43,27 +79,18 @@ fn generate_default_config(overwrite:bool) -> Result<(), serde::yaml::Error> {
 
     // Insert code for serializing variable
     let s = serde_yaml::to_string(&settings)?;
-    
+
     // Creates file in directory
-    let f = File::create(&path);
-
-    // match statement, if file creates
-    // let f = match f {
-    //     Ok(file) => file,
-    //     Err(error) => panic!("This file already exists {:?}", error),
-    // };
-
-    let f = match f {
-        Ok(file) => file,
+    let f = match std::fs::File::create(&path) {
+        Ok(file) => Ok(file),
         Err(error) => match error.kind() {
-            ErrorKind::AlreadyExists => match File::open(path) {
-                Ok(fo) => {
-                    if overwrite == True {
-                        fo,
-                    };
-                }
-                Err(err) => panic!("This file already exists {:?}", err);
-            }
+            ErrorKind::AlreadyExists if overwrite => Ok(OpenOptions::new().write(true).truncate(true).open(path)?),
+            ErrorKind::AlreadyExists => Err(WriteConfigError::AlreadyExists),
+            _ => Err(WriteConfigError::IoError(error)),
         },
-    };
+    }?;
+
+    f.write_all(&s.as_bytes())?;
+
+    Ok(())
 }
